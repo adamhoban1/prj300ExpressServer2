f54032271bd4451e524385fc35b0f6e526ab0f38
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-11-12 11:04:13 +0000

    added model for elert

---
+++ src/models/alert.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb";
+import { z } from "zod";
+
+export interface alert {
+    _id?: ObjectId;
+    category: string;
+    severity: string;
+    notes: string;
+    location: string;
+    Reportedby: string;
+    datecreated?: Date;
+}
+
+/**
+export const createUserSchema = z.object({
+    username: z.string().min(1).regex(/^[a-zA-ZÀ-ÿ0-9'_-]+$/), // Letters, spaces and numbers
+    password: z.string().min(6),
+    phonenumber: z.string().regex(/^(?:\+353|0)87\d{7}$/), // E.164 format 
+    email: z.email()
+});
+
+export const updateUserSchema = z.object({
+  username: z.string().min(1).regex(/^[a-zA-ZÀ-ÿ0-9'_-]+$/).optional(), // Only letters and spaces
+    password: z.string().min(6).optional(),
+    phonenumber: z.string().regex(/^(?:\+353|0)87\d{7}$/).optional(), // E.164 format 
+    email: z.email().optional()
+});
+*/
