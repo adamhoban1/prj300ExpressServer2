58fe6363b287266362d5c38fd1e83af904cdb5e6
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-11-12 10:30:11 +0000

    added new updated create and delete and get

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { User } from '../models/user';
 import { collections } from '../database';
+import { ObjectId } from 'mongodb';
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
@@ -25,30 +26,52 @@ export const getUsers = async (req: Request, res: Response) => {
       res.status(500).send("Failed to get users.");
     }
 };
+export const getUserById = async (req: Request, res: Response) => {
+  //get a single  user by ID from the database
 
+  let id: string = req.params.id;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const user = (await collections.users?.findOne(query)) as unknown as User;
 
-export const getUserById = (req: Request, res: Response) => {
-    let id:string = req.params.id;
-    res.json({"message": `get a user ${id} received`})
+    if (user) {
+      res.status(200).send(user);
+    }
+    else {
+      res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+  } catch (error) {
+    if (error instanceof Error) 
+      { 
+        console.log(`Unable to find matching document with id: ${error.message}`);
+      }
+      else{
+        console.log(`error with ${error}`)
+      }
+      res.status(404).send(`Unable to find matching document with id 2: ${req.params.id}`);
+    }
 };
+
 
 export const createUser = async (req: Request, res: Response) => {
-    console.log(req.body);
-    const newUser = req.body as User; 
-    try {
-        const result = await collections.users?.insertOne(newUser)
-        if (result) {
-            res.status(201).location(`${result.insertedId}`).json({ message: `Created a new user with id ${result.insertedId}` })
-        }
-        else {
-            res.status(500).send("Failed to create a new user.");
-        }
+  console.log(req.body);
+
+  const newUser = req.body as User;
+  try {
+    const result = await collections.users?.insertOne(newUser)
+    if (result) {
+      res.status(201).location(`${result.insertedId}`).json({ message: `Created a new user with id ${result.insertedId}` })
     }
-    catch (error) {
-        console.error(error);
-        res.status(400).send(`Unable to create new user`);
+    else {
+      res.status(500).send("Failed to create a new user.");
     }
+  }
+  catch (error) {
+    console.error(error);
+    res.status(400).send(`Unable to create new user`);
+  }
 };
+
 
 
 export const updateUser = (req: Request, res: Response) => {
@@ -56,6 +79,23 @@ export const updateUser = (req: Request, res: Response) => {
     res.json({"message": `update user ${req.params.id} with data from the post message`})
 };
 
-export const deleteUser = (req: Request, res: Response) => {
-    res.json({"message": `delete user ${req.params.id} from the database`})
+export const deleteUser = async(req: Request, res: Response) => {
+  let id: string = req.params.id;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const result = await collections.users?.deleteOne(query);
+
+    if (result && result.deletedCount === 1) {
+      res.status(200).json({"message": `Deleted user ${req.params.id} from the database`});
+    } else {
+      res.status(404).send(`User with id ${req.params.id} not found.`);
+    }
+  } catch (error) {
+    if (error instanceof Error) { 
+      console.log(`Unable to delete user with id: ${error.message}`);
+    } else {
+      console.log(`error with ${error}`);
+    }
+    res.status(500).send(`Unable to delete user ${req.params.id}`);
+  }
 };
