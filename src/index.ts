47e78b60609a5ac1f75bed190326d480166b09b9
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2025-11-12 10:33:48 +0000

    added middleware authenciate

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import userRoutes from "./routes/users";//import user routes
 import dotenv from "dotenv";
 import { initDb } from "./database";
+import { authenticateKey } from "./middleware/auth.middleware";
 
 dotenv.config();
 
@@ -14,7 +15,8 @@ const app: Application = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/v1/users", userRoutes );//api base path
-
+//app.use("/api/v1/users", userRoutes );//middleware authenciate version 
+app.use(authenticateKey);
 app.get("/ping", async (_req : Request, res: Response) => {//for Project 300 testing purposes
     res.json({
         message: "Test for express server is successful",
