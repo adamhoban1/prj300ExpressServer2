f25aca1ff8de8b200642a14781986ed13fdaac67
adamhoban1 <158476444+adamhoban1@users.noreply.github.com>
2026-01-19 20:16:27 +0000

    failed to add notifacations

--- src/Routes/alert.ts
+++ src/Routes/alert.ts
@@ -2,6 +2,7 @@ import express, {Router} from 'express';
 import { getReports, getReportById, createReport, updateReport, deleteReport } from '../controllers/Report';
 import { validate } from '../middleware/validate.middleware';
 import { createReportSchema, updateReportSchema } from '../models/Report';
+import { sendPushNotification } from "../services/push.service";
 
 
 const router: Router = express.Router();
@@ -11,4 +12,15 @@ router.get('/:id', getReportById);
 router.post('/', validate(createReportSchema), createReport);
 router.put('/:id', validate(updateReportSchema), updateReport);
 router.delete('/:id', deleteReport);
+router.post("/test-push", async (req, res) => {
+  const { token } = req.body;
+
+  await sendPushNotification(
+    [token],
+    "Test Alert 🚨",
+    "Push notifications are working!"
+  );
+
+  res.json({ message: "Notification sent" });
+});
 export default router;
